a7e8cc39de7c1b085195411021d70eb309398f87
Tojan-Naiem <tojan050@gmail.com>
2026-02-05 19:51:23 +0200

    patch : add changes in export_text

--- Models/export_text_embeddings.py
+++ Models/export_text_embeddings.py
@@ -6,15 +6,7 @@ device = "cpu"
 model, preprocess = clip.load("ViT-B/32", device=device)
 
 labels = [
-       "a screenshot of a web browser",
-    "a screenshot of code editor or terminal",
-    "a screenshot of a chat or messaging app",
-    "a screenshot of a game",
-    "a screenshot of a document or spreadsheet",
-    "a screenshot of social media",
-    "a screenshot of system settings",
-    "a screenshot of an error message",
-    "a screenshot of something else"
+    "Person","Documents","Code", "Browser","Chat","Games", "other"
     ]
 text = clip.tokenize(labels).to(device)
 
